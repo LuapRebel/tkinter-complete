2fc252d20831c23436194c7792488f19646804cf
Paul Leber <paulleber23@gmail.com>
2023-04-07 12:26:13 -0700

    update calculations

--- bmi/bmi.py
+++ bmi/bmi.py
@@ -21,13 +21,29 @@ class App(ctk.CTk):
         self.columnconfigure(0, weight=1)
         self.rowconfigure((0, 1, 2, 3), weight=1, uniform="a")
 
+        # data
+        self.height_int = ctk.IntVar(value=170)
+        self.weight_float = ctk.DoubleVar(value=65)
+        self.bmi_string = ctk.StringVar()
+        self.update_bmi()
+
+        # tracing
+        self.height_int.trace("w", self.update_bmi)
+        self.weight_float.trace("w", self.update_bmi)
+
         # widgets
-        ResultText(self)
-        WeightInput(self)
-        HeightInput(self)
+        ResultText(self, self.bmi_string)
+        WeightInput(self, self.weight_float)
+        HeightInput(self, self.height_int)
         UnitSwitcher(self)
 
         self.mainloop()
+
+    def update_bmi(self, *args):
+        height_meter = self.height_int.get() / 100
+        weight_kg = self.weight_float.get()
+        bmi_result = round(weight_kg / height_meter**2, 2)
+        self.bmi_string.set(bmi_result)
 
     def change_title_bar_color(self):
         try:
@@ -43,16 +59,28 @@ class App(ctk.CTk):
 
 
 class ResultText(ctk.CTkLabel):
-    def __init__(self, parent):
+    def __init__(self, parent, bmi_string):
         font = ctk.CTkFont(family=FONT, size=MAIN_TEXT_SIZE, weight="bold")
-        super().__init__(master=parent, text=22.5, font=font, text_color=WHITE)
+        super().__init__(
+            master=parent,
+            text=22.5,
+            font=font,
+            text_color=WHITE,
+            textvariable=bmi_string,
+        )
         self.grid(column=0, row=0, rowspan=2, sticky="nsew")
 
 
 class WeightInput(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, weight_float):
         super().__init__(master=parent, fg_color=WHITE)
         self.grid(column=0, row=2, sticky="nsew", padx=10, pady=10)
+        self.weight_float = weight_float
+
+        # exercise
+        # update text to display current weight
+        self.output_string = ctk.StringVar()
+        self.update_weight()
 
         # layout
         self.rowconfigure(0, weight=1, uniform="a")
@@ -64,7 +92,12 @@ class WeightInput(ctk.CTkFrame):
 
         # font
         font = ctk.CTkFont(family=FONT, size=INPUT_FONT_SIZE)
-        label = ctk.CTkLabel(self, text="70kg", text_color=BLACK, font=font)
+        label = ctk.CTkLabel(
+            self,
+            textvariable=self.output_string,
+            text_color=BLACK,
+            font=font,
+        )
         label.grid(row=0, column=2)
 
         # buttons
@@ -76,6 +109,7 @@ class WeightInput(ctk.CTkFrame):
             fg_color=LIGHT_GRAY,
             hover_color=GRAY,
             corner_radius=BUTTON_CORNER_RADIUS,
+            command=lambda: self.update_weight(("minus", "large")),
         )
         minus_button.grid(row=0, column=0, sticky="ns", padx=8, pady=8)
 
@@ -87,6 +121,7 @@ class WeightInput(ctk.CTkFrame):
             fg_color=LIGHT_GRAY,
             hover_color=GRAY,
             corner_radius=BUTTON_CORNER_RADIUS,
+            command=lambda: self.update_weight(("plus", "large")),
         )
         plus_button.grid(row=0, column=4, sticky="ns", padx=8, pady=8)
 
@@ -98,10 +133,11 @@ class WeightInput(ctk.CTkFrame):
             fg_color=LIGHT_GRAY,
             hover_color=GRAY,
             corner_radius=BUTTON_CORNER_RADIUS,
+            command=lambda: self.update_weight(("plus", "small")),
         )
         small_plus_button.grid(row=0, column=3, padx=4, pady=4)
 
-        small_plus_button = ctk.CTkButton(
+        small_minus_button = ctk.CTkButton(
             self,
             text="-",
             font=font,
@@ -109,32 +145,55 @@ class WeightInput(ctk.CTkFrame):
             fg_color=LIGHT_GRAY,
             hover_color=GRAY,
             corner_radius=BUTTON_CORNER_RADIUS,
+            command=lambda: self.update_weight(("minus", "small")),
         )
-        small_plus_button.grid(row=0, column=1, padx=4, pady=4)
+        small_minus_button.grid(row=0, column=1, padx=4, pady=4)
+
+    def update_weight(self, info=None):
+        if info:
+            amount = 1 if info[1] == "large" else 0.1
+            if info[0] == "plus":
+                self.weight_float.set(self.weight_float.get() + amount)
+            else:
+                self.weight_float.set(self.weight_float.get() - amount)
+        self.output_string.set(f"{round(self.weight_float.get(), 1)}kg")
 
 
 class HeightInput(ctk.CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, height_int):
         super().__init__(master=parent, fg_color=WHITE)
         self.grid(row=3, column=0, sticky="nsew", padx=10, pady=10)
 
         # widgets
         slider = ctk.CTkSlider(
             master=self,
+            command=self.update_text,
             button_color=GREEN,
             button_hover_color=GRAY,
             progress_color=GREEN,
             fg_color=LIGHT_GRAY,
+            variable=height_int,
+            from_=100,
+            to=250,
         )
         slider.pack(side="left", fill="x", expand=True, padx=10, pady=10)
 
+        self.output_string = ctk.StringVar()
+        self.update_text(amount=height_int.get())
+
         output_text = ctk.CTkLabel(
             self,
-            text="1.80m",
             text_color=BLACK,
             font=ctk.CTkFont(family=FONT, size=INPUT_FONT_SIZE),
+            textvariable=self.output_string,
         )
         output_text.pack(side="left", padx=20)
+
+    def update_text(self, amount):
+        text_string = str(int(amount))
+        meter = text_string[0]
+        cm = text_string[1:]
+        self.output_string.set(f"{meter}.{cm}m")
 
 
 class UnitSwitcher(ctk.CTkLabel):
